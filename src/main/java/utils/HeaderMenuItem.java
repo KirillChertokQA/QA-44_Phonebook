61f992994d36cb7cb0a1c9b0fe74d8732d76d588
KirillChertokQA <prefixspb@gmail.com>
2024-09-20 12:05:42 +0300

    created add new contact

---
+++ src/main/java/utils/HeaderMenuItem.java
@@ -0,0 +1,26 @@
+package utils;
+
+public enum HeaderMenuItem {
+
+    HOME("//a[text()='HOME']"),
+
+    ABOUT("//a[text()='ABOUT']"),
+
+    LOGIN("//a[text()='LOGIN']"),
+
+    CONTACTS("//a[text()='CONTACTS']"),
+
+    ADD("//a[text()='ADD']"),
+
+    SIGN_OUT("//button[text()='Sign Out']");;
+
+    private final String locator;
+
+    HeaderMenuItem(String locator) {
+        this.locator = locator;
+    }
+
+    public String getLocator(){
+        return locator;
+    }
+}
